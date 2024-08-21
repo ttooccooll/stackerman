71db17cfb3ab7f2abdd58a0354dd9f8cb02ae14c
Jason <jasonbohio@gmail.com>
2024-08-21 10:49:48 -0400

    v

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.use(express.static(path.join(__dirname)));
 
+app.use('/static', express.static(path.join(__dirname, 'node_modules')));
+
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
